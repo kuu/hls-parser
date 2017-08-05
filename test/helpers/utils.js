c28002639a5d4591311b3256af20c8b4b0435cd8
Kuu Miyazaki <miyazaqui@gmail.com>
2017-08-05 14:26:39 +0900

    Support Redundant Streams

--- test/helpers/utils.js
+++ test/helpers/utils.js
@@ -45,10 +45,53 @@ function stringifyFail(t, obj) {
   t.fail('HLS.stringify() did not fail');
 }
 
+function stripSpaces(text) {
+  const chars = [];
+  let insideDoubleQuotes = false;
+  for (const ch of text) {
+    if (ch === '"') {
+      if (insideDoubleQuotes) {
+        insideDoubleQuotes = false;
+      } else {
+        insideDoubleQuotes = true;
+      }
+    } else if (ch === ' ') {
+      if (!insideDoubleQuotes) {
+        continue;
+      }
+    }
+    chars.push(ch);
+  }
+  return chars.join('');
+}
+
+function stripCommentsAndEmptyLines(text) {
+  const lines = [];
+  text.split('\n').forEach(l => {
+    const line = l.trim();
+    if (!line) {
+      // empty line
+      return;
+    }
+    if (line.startsWith('#')) {
+      if (line.startsWith('#EXT')) {
+        // tag
+        return lines.push(stripSpaces(line));
+      }
+      // comment
+      return;
+    }
+    // uri
+    lines.push(line);
+  });
+  return lines.join('\n');
+}
+
 module.exports = {
   parsePass,
   stringifyPass,
   bothPass,
   parseFail,
-  stringifyFail
+  stringifyFail,
+  stripCommentsAndEmptyLines
 };
--- test/spec/stringify.spec.js
+++ test/spec/stringify.spec.js
@@ -1,32 +1,11 @@
 const test = require('ava');
 const fixtures = require('../helpers/fixtures');
+const utils = require('../helpers/utils');
 const HLS = require('../../');
 
 fixtures.forEach(({name, m3u8, object}) => {
   test(name, t => {
     const result = HLS.stringify(object);
-    t.is(result, stripCommentsAndEmptyLines(m3u8));
+    t.is(result, utils.stripCommentsAndEmptyLines(m3u8));
   });
 });
-
-function stripCommentsAndEmptyLines(text) {
-  const lines = [];
-  text.split('\n').forEach(l => {
-    const line = l.trim();
-    if (!line) {
-      // empty line
-      return;
-    }
-    if (line.startsWith('#')) {
-      if (line.startsWith('#EXT')) {
-        // tag
-        return lines.push(line);
-      }
-      // comment
-      return;
-    }
-    // uri
-    lines.push(line);
-  });
-  return lines.join('\n');
-}
