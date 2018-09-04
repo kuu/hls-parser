db385719fc3419733c31fa840d7b38c0d1738085
kmiyazaki <kmiyazaki@ooyala.com>
2018-09-04 10:29:38 +0900

    Fix webpack workflow

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,35 @@
 const path = require('path');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
-module.exports = {
-  entry: {
-    'hls-parser': './index.js',
-    'hls-parser.min': './index.js'
-  },
-  devtool: 'source-map',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js',
-    library: 'HLS',
-    libraryTarget: 'umd'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env']
-        }
-      }
-    ]
-  },
-  optimization: {
-    minimizer: [
-      new UglifyJSPlugin({
-        uglifyOptions: {
-          compress: {
-            drop_console: true
+module.exports = (_, argv) => {
+  return {
+    entry: './index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: `hls-parser${argv.mode === 'production' ? '.min' : ''}.js`,
+      library: 'HLS',
+      libraryTarget: 'umd'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
           }
         }
-      })
-    ]
-  }
+      ]
+    },
+    devtool: argv.mode === 'production' ? 'source-map' : 'none',
+    optimization: {
+      minimize: argv.mode === 'production',
+      minimizer: [
+        new UglifyJSPlugin({
+          sourceMap: true
+        })
+      ]
+    }
+  };
 };
