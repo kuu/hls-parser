86c47da839abe7c5417684cb640a16a9e02c3225
Kuu Miyazaki <miyazaqui@gmail.com>
2019-07-16 22:26:19 +0900

    Allow multiple ad breaks (Update some tests) (#54)

--- test/fixtures/objects/SCTE-35_06.js
+++ test/fixtures/objects/SCTE-35_06.js
@@ -25,49 +25,83 @@ function createSegments() {
     discontinuitySequence: 0,
     markers: [{
       type: 'OUT',
-      duration: 15.0
+      duration: 23.0
     }]
   }));
   segments.push(new Segment({
     uri: '3.ts',
-    duration: 7,
+    duration: 8,
     title: '',
     mediaSequenceNumber: 2,
-    discontinuitySequence: 0
+    discontinuitySequence: 0,
+    markers: [{
+      type: 'RAW',
+      tagName: 'EXT-X-CUE-OUT-CONT',
+      value: 'ElapsedTime=8,Duration=23'
+    }]
   }));
   segments.push(new Segment({
     uri: '4.ts',
-    duration: 8.008,
+    duration: 7,
     title: '',
     mediaSequenceNumber: 3,
+    discontinuitySequence: 0,
+    markers: [{
+      type: 'RAW',
+      tagName: 'EXT-X-CUE-OUT-CONT',
+      value: 'ElapsedTime=16,Duration=23'
+    }]
+  }));
+  segments.push(new Segment({
+    uri: '5.ts',
+    duration: 8.008,
+    title: '',
+    mediaSequenceNumber: 4,
     discontinuitySequence: 0,
     markers: [{
       type: 'IN'
     }]
   }));
   segments.push(new Segment({
-    uri: '5.ts',
+    uri: '6.ts',
     duration: 8,
     title: '',
-    mediaSequenceNumber: 4,
+    mediaSequenceNumber: 5,
     discontinuitySequence: 0,
     markers: [{
       type: 'OUT',
-      duration: 15.0
+      duration: 23.0
     }]
   }));
   segments.push(new Segment({
-    uri: '6.ts',
-    duration: 7,
-    title: '',
-    mediaSequenceNumber: 5,
-    discontinuitySequence: 0
-  }));
-  segments.push(new Segment({
     uri: '7.ts',
-    duration: 8.008,
+    duration: 8,
     title: '',
     mediaSequenceNumber: 6,
+    discontinuitySequence: 0,
+    markers: [{
+      type: 'RAW',
+      tagName: 'EXT-X-CUE-OUT-CONT',
+      value: 'ElapsedTime=8,Duration=23'
+    }]
+  }));
+  segments.push(new Segment({
+    uri: '8.ts',
+    duration: 7,
+    title: '',
+    mediaSequenceNumber: 7,
+    discontinuitySequence: 0,
+    markers: [{
+      type: 'RAW',
+      tagName: 'EXT-X-CUE-OUT-CONT',
+      value: 'ElapsedTime=16,Duration=23'
+    }]
+  }));
+  segments.push(new Segment({
+    uri: '9.ts',
+    duration: 8.008,
+    title: '',
+    mediaSequenceNumber: 8,
     discontinuitySequence: 0,
     markers: [{
       type: 'IN'
