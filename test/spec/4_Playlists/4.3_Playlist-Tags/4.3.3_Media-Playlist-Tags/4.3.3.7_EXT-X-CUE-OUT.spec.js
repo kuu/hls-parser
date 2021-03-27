ebb85260b7a972176b36283742e5a760af650452
Kuu Miyazaki <miyazaqui@gmail.com>
2021-03-27 10:46:53 +0900

    Routines/add tests (#102)
    * Add test
    
    * Update dependencies

--- test/spec/4_Playlists/4.3_Playlist-Tags/4.3.3_Media-Playlist-Tags/4.3.3.7_EXT-X-CUE-OUT.spec.js
+++ test/spec/4_Playlists/4.3_Playlist-Tags/4.3.3_Media-Playlist-Tags/4.3.3.7_EXT-X-CUE-OUT.spec.js
@@ -2,7 +2,7 @@ const test = require('ava');
 const utils = require('../../../../helpers/utils');
 
 test('#EXT-X-CUE-OUT_01', t => {
-  utils.parsePass(t, `
+  let obj = utils.parsePass(t, `
     #EXTM3U
     #EXT-X-TARGETDURATION:10
     #EXT-X-VERSION:3
@@ -12,7 +12,8 @@ test('#EXT-X-CUE-OUT_01', t => {
     #EXTINF:10,
     http://example.com/2
   `);
-  utils.parsePass(t, `
+  t.is(obj?.segments[1]?.markers[0]?.duration, 30);
+  obj = utils.parsePass(t, `
     #EXTM3U
     #EXT-X-TARGETDURATION:10
     #EXT-X-VERSION:3
@@ -22,4 +23,5 @@ test('#EXT-X-CUE-OUT_01', t => {
     #EXTINF:10,
     http://example.com/2
   `);
+  t.is(obj?.segments[1]?.markers[0]?.duration, 30);
 });
