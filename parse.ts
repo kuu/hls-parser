308b23f93d4b9d561cfce6729be587b9ec53cc29
Kuu Miyazaki <miyazaqui@gmail.com>
2024-03-17 21:04:24 +0900

    Update dev dependencies

--- parse.ts
+++ parse.ts
@@ -900,9 +900,7 @@ function checkLowLatencyCompatibility({lowLatencyCompatibility, targetDuration, 
   }
   for (const report of renditionReports) {
     const lastSegment = segments.at(-1);
-    if (report.lastMSN === null || report.lastMSN === undefined) {
-      report.lastMSN = lastSegment.mediaSequenceNumber;
-    }
+    report.lastMSN ??= lastSegment.mediaSequenceNumber;
     if ((report.lastPart === null || report.lastPart === undefined) && lastSegment.parts.length > 0) {
       report.lastPart = lastSegment.parts.length - 1;
     }
